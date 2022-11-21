43dd525ad0e88ffed81a3e65409381d425ffd336
martin <nozistance@gmail.com>
2022-11-21 22:47:47 +0300

    Added directory-type resource packs support

--- src/main/java/net/javahub/rose/resources/ResourcesProvider.java
+++ src/main/java/net/javahub/rose/resources/ResourcesProvider.java
@@ -3,12 +3,13 @@ package net.javahub.rose.resources;
 import net.minecraft.resource.*;
 
 import java.io.File;
+import java.util.Objects;
 import java.util.Set;
 import java.util.function.Consumer;
+import java.util.function.Supplier;
 import java.util.stream.Collectors;
 
 import static net.javahub.rose.Rose.CONFIG;
-import static net.javahub.rose.Rose.LOGGER;
 
 public class ResourcesProvider implements ResourcePackProvider {
 
@@ -16,22 +17,21 @@ public class ResourcesProvider implements ResourcePackProvider {
         return CONFIG.sources().stream().map(File::new).collect(Collectors.toSet());
     }
 
+    private Supplier<ResourcePack> getResourceResource(File file) {
+        return file.isDirectory() ?
+                () -> new DirectoryResourcePack(file) :
+                () -> new ZipResourcePack(file);
+    }
+
+    private ResourcePackProfile registerResourcePack(String id, File file, ResourcePackProfile.Factory factory) {
+        return ResourcePackProfile.of(id, false, getResourceResource(file), factory,
+                ResourcePackProfile.InsertionPosition.TOP, ResourcePackSource.PACK_SOURCE_NONE);
+    }
+
     @Override
     public void register(Consumer<ResourcePackProfile> profileAdder, ResourcePackProfile.Factory factory) {
         Set<File> files = getSources();
-        files.stream().filter(f -> registerResourcePack(String.valueOf(f.hashCode()), f, profileAdder, factory))
-                .map(File::toString).forEach(LOGGER::info);
-    }
-
-    private boolean registerResourcePack(
-            String id, File resources, Consumer<ResourcePackProfile> profileAdder, ResourcePackProfile.Factory factory) {
-        boolean answer = false;
-        ResourcePackProfile container = ResourcePackProfile.of(id , false, () -> new ZipResourcePack(resources),
-                factory, ResourcePackProfile.InsertionPosition.TOP, ResourcePackSource.PACK_SOURCE_NONE);
-        if (container != null) {
-            profileAdder.accept(container);
-            answer = true;
-        }
-        return answer;
+        files.stream().map(f -> registerResourcePack(String.valueOf(f.hashCode()), f, factory))
+                .filter(Objects::nonNull).forEach(profileAdder);
     }
 }
